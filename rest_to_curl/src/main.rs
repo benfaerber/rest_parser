d5c9a929af17b44ca2b88c4ed9d3d5b995c4d0f9
Ben Faerber <benfaerber@protonmail.com>
2024-12-01 14:17:46 -0700

    Add variables

--- rest_to_curl/src/main.rs
+++ rest_to_curl/src/main.rs
@@ -1,4 +1,4 @@
-use rest_parser::{RestFormat, RestRequest, RestVariables, IndexMap, Template, Body};
+use rest_parser::{Body, IndexMap, RestFormat, RestRequest, RestVariables, Template, TemplatePart};
 use std::fs;
 
 struct CurlRenderer {
@@ -14,7 +14,7 @@ impl CurlRenderer {
 
     fn render_query(&self, query: IndexMap<String, Template>) -> String {
         let params = query.iter()
-            .map(|(k, v)| format!("{}={}", k, v.render(&self.vars)))
+            .map(|(k, v)| format!("{}={}", k, self.render_template(v)))
             .collect::<Vec<String>>()
             .join("&");
 
@@ -29,7 +29,8 @@ impl CurlRenderer {
         let filepath = filepath.render(&self.vars); 
         let raw = fs::read_to_string(&filepath).expect("Invalid file!");
         if process_variables {
-            Template::new(&raw).render(&self.vars)
+            let template = Template::new(&raw);
+            self.render_template(&template)
         } else {
             raw
         }
@@ -38,18 +39,18 @@ impl CurlRenderer {
     fn render_body(&self, opt_body: Option<Body>) -> (String, String) {
         let mut save_to = None; 
         let rendered_body = opt_body.map(|body| match body {
-            Body::Text(text) => text.render(&self.vars),
+            Body::Text(text) => self.render_template(&text), 
             Body::LoadFromFile { filepath, process_variables, .. } => self.load_body_from_file(filepath, process_variables),
             Body::SaveToFile { text, filepath } => {
-                save_to = Some(filepath.render(&self.vars));
-                text.render(&self.vars)
+                save_to = Some(self.render_template(&filepath));
+                self.render_template(&text)
             },
         });
        
         let out_body = match rendered_body {
             Some(body_text) => {
                 let encoded_body = body_text
-                    .replace("\r\n", "")
+                    .replace("&\r\n", "")
                     .replace("\n", "")
                     .replace("\"", "\\\"");
                 format!(" -d \"{}\"", encoded_body)
@@ -68,30 +69,45 @@ impl CurlRenderer {
     fn render_headers(&self, headers: IndexMap<String, Template>) -> String {
         let all_headers = headers
             .iter()
-            .map(|(k, v)| format!("-H \"{}: {}\"", k, v.render(&self.vars)))
+            .map(|(k, v)| format!("-H \"{}: {}\"", k, self.render_template(v)))
             .collect::<Vec<String>>()
             .join(" ");
         format!(" {all_headers}")
     }
 
     fn render_url(&self, url: Template) -> String {
-        url.render(&self.vars)
+        self.render_template(&url) 
     }
 
     fn render_method(&self, method: Template) -> String {
-        let method = method.render(&self.vars);
+        let method = self.render_template(&method); 
         format!(" -X {method}")
+    }
+
+    fn render_variables(&self) -> String {
+        let all_vars = self.vars.iter().map(|(k, v)| {
+            format!("{}={}", k, self.render_template(v)) 
+        }).collect::<Vec<String>>().join("; ");
+        format!("{all_vars}; ")
+    }
+
+    fn render_template(&self, template: &Template) -> String {
+        template.parts.iter().map(|part| match part {
+            TemplatePart::Text(text) => text.clone(),
+            TemplatePart::Variable(var) => format!("${var}"),
+        }).collect::<Vec<String>>().join("")
     }
 
     fn render_request(&self, req: RestRequest) -> String {
         let RestRequest { headers, method, query, body, url, .. } = req; 
+        let variables = self.render_variables(); 
         let headers = self.render_headers(headers);
         let method = self.render_method(method); 
         let query = self.render_query(query);
         let (body, output) = self.render_body(body);
         let url = self.render_url(url);
         
-        format!("curl {url}{query}{method}{output}{headers}{body}")
+        format!("{variables}curl {url}{query}{method}{output}{headers}{body}")
     }
 }
 
